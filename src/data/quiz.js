91867d609f8d405389eedefb3f4dc06a4932b00b
市川凌大 <ichikawaryohta@MacBook-Air.local>
2025-09-20 08:53:37 +0900

    create quizDisplay

---
+++ src/data/quiz.js
@@ -0,0 +1,47 @@
+
+
+const quizData = [
+    {
+        question: "Question 1",
+        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        AnswerIndex: 0
+    },{
+        question: "Question 2",
+        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        AnswerIndex: 1
+    },{
+        question: "Question 3",
+        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        AnswerIndex: 2
+    },{
+        question: "Question 4",
+        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        AnswerIndex: 3
+    },{
+        question: "Question 5",
+        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        AnswerIndex: 0
+    },{
+        question: "Question 6",
+        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        AnswerIndex: 1
+    },{
+        question: "Question 7",
+        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        AnswerIndex: 2
+    },{
+        question: "Question 8",
+        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        AnswerIndex: 3
+    },{
+        question: "Question 9",
+        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        AnswerIndex: 0
+    },{
+        question: "Question 10",
+        options: ["Select 1", "Select 2", "Select 3", "Select 4"],
+        AnswerIndex: 1
+    }
+]
+
+export default quizData;
--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,5 +1,13 @@
+import Display from "../components/Display/Display"
+import quizData from "../data/quiz"
+
 export default function QuizPage() {
+    const quizIndex = 0;
   return (
-    <div>QuizPage</div>
+    <>
+        <Display>
+            {`Q1. ${quizData[quizIndex].question}`}
+        </Display>
+    </>
   )
 }
