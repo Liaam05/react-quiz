8f0997e2404a89ce7b34b9c225091fe23832367d
市川凌大 <ichikawaryohta@MacBook-Air.local>
2025-09-20 10:10:19 +0900

    create Quizpage

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../components/Button/Button";
 import Display from "../components/Display/Display"
 import quizData from "../data/quiz"
+import { ROUTES } from "../const";
+import { useNavigate } from "react-router-dom";
 
 export default function QuizPage() {
     const [quizIndex, setQuizIndex] = useState(0);
     const [answerLogs, setAnswerLogs] = useState([]);
+    const navigation = useNavigate();
+    const MAX_QUIZ_LEN = quizData.length;
 
     const handleClick = (clickedIndex) => {
         if(clickedIndex === quizData[quizIndex].AnswerIndex){
@@ -15,12 +19,28 @@ export default function QuizPage() {
         }
         setQuizIndex((prev) => prev + 1);
     };
+
+    useEffect(() => {
+        if(answerLogs.length === MAX_QUIZ_LEN){
+            const correctNum = answerLogs.filter((answer) => {
+                return answer === true;
+            })
+            navigation(ROUTES.RESULT, {
+                state: {
+                    maxQuizLen: MAX_QUIZ_LEN,
+                    correctNum: correctNum
+                }
+            });
+        }
+
+    },[answerLogs, MAX_QUIZ_LEN, navigation]);
+
   return (
     <>
-        <Display>
+        {quizData[quizIndex] && <Display>
             {`Q1. ${quizData[quizIndex].question}`}
-        </Display>
-        {
+        </Display>}
+        {quizData[quizIndex] &&
             quizData[quizIndex].options.map((option, index) => {
                 return (
                     <Button key={`option-${index}`} onClick={() => handleClick(index)}>
